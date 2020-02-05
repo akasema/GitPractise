75ecd6cb270c640e21e4811d382c771204d63664
Kadir Gok <kadirgok1@gmail.com>
2020-02-04 21:17:11 -0500

    new added file

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -1,5 +1,18 @@
 package test;
 
 public class DemoClass {
+<<<<<<< Updated upstream
 dddd remote change 
+=======
+	public static void main(String[] args) {
+		
+	
+	for (int i=0; i<=10; i++) {
+		System.out.println(i);
+	}
+		System.out.println("hello world" );
+>>>>>>> Stashed changes
+
+System.out.println();
+}
 }
