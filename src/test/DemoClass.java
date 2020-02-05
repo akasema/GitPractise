ae2225ee66725882eb6c634b57813dca285b69fc
Kadir Gok <kadirgok1@gmail.com>
2020-02-04 21:23:17 -0500

    newwww

--- src/test/DemoClass.java
+++ src/test/DemoClass.java
@@ -2,8 +2,11 @@ package test;
 
 public class DemoClass {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 dddd remote change 
 =======
+=======
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		
 	
@@ -11,8 +14,11 @@ dddd remote change
 		System.out.println(i);
 	}
 		System.out.println("hello world" );
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
 
 System.out.println();
+=======
+>>>>>>> Stashed changes
 }
 }
